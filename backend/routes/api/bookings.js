c34a2885e1d8e450724c2ed813539b696769eb90
Cheryn Shin <cherynjjaeshin@gmail.com>
2023-05-25 18:26:26 -0700

    added bookings router

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { Op, Sequelize } = require('sequelize');
+const { User, Spot, Review, Image, Booking } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+
+
+module.exports = router;
