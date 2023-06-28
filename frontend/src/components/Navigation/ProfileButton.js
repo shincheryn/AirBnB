87de84a140d734f445a8b0467ef65dfbf7462772
Cheryn Shin <cherynjjaeshin@gmail.com>
2023-06-28 15:56:41 -0500

    Dropdown menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -5,20 +5,21 @@ import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
-  const ulClassName = "profile-dropdown";
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
     <>
-      <button>
+      <button onClick={() => setShowMenu(!showMenu)}>
         <i className="fas fa-user-circle" />
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
@@ -28,6 +29,6 @@ function ProfileButton({ user }) {
       </ul>
     </>
   );
-}ProfileButton component
+}
 
 export default ProfileButton;
